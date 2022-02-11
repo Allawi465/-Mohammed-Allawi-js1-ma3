7c2774d55adb15026848f79a1607e6f0215e1820
Allawi465 <allawi465@gmail.com>
2022-02-11 03:29:33 +0100

    .

---
+++ js/question-2.js
@@ -0,0 +1,36 @@
+// 
+
+const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating&key=06afb03635514b43ab0653af92e562b2";
+
+const resultsContainer = document.querySelector(".results");
+
+async function getInfo(key) {
+
+     const response = await fetch(url);
+
+     const result = await response.json();
+
+     const facts = result.results;
+
+     console.log(facts);
+
+     resultsContainer.innerHTML = "";
+
+     for (let i = 0; i < facts.length; i++) {
+
+        if (i === 8) {
+            break;
+        }
+
+        resultsContainer.innerHTML += `<div class="result">
+                                        ${facts[i].name}
+                                        ${facts[i].rating}
+                                        </div>`;
+    }
+};
+
+getInfo();
+
+/* function displayError(message = "Unknown error") {
+    return `<div class="error">${message}</div>`;
+}; */
